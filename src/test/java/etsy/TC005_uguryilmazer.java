8123e16bc2949a6efc814f8b9001da1b08af2526
Ugur Yilmazer <kobra667788@gmail.com>
2021-07-02 23:50:33 +0300

    TC005 work2

--- src/test/java/etsy/TC005_uguryilmazer.java
+++ src/test/java/etsy/TC005_uguryilmazer.java
@@ -2,11 +2,15 @@ package etsy;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.BrowserFactory;
+
+import java.util.List;
 
 public class TC005_uguryilmazer {
     WebDriver driver ;
@@ -21,9 +25,14 @@ public class TC005_uguryilmazer {
     //driver.close();
     }
     @Test
-    public void testcase001(){
+    public void testcase005(){
     driver.get("https://www.etsy.com/");
     driver.findElement(By.cssSelector("#catnav-primary-link-11049")).click();
+        //List<WebElement> color1 = driver.findElements(By.xpath("//input[@name='attr_1']"));
+        //System.out.println("color1.size() = " + color1.size());
+
+        WebElement element = driver.findElement(By.xpath("//label[@for='attr_1-4']"));
+        Assert.assertFalse(element.isDisplayed());
 
 
     }
