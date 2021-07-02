4d8c8d405ac21d7ba2985d5707341dc6ad263d02
Umit Olmez <umitolmez455@gmail.com>
2021-07-02 13:00:25 +0300

    TC009 is passed successfully

--- src/test/java/etsy/TC009_UmitOlmez.java
+++ src/test/java/etsy/TC009_UmitOlmez.java
@@ -3,8 +3,13 @@ package etsy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.*;
 import utilities.BrowserFactory;
+
+import java.util.Set;
+
+import static org.testng.Assert.assertTrue;
 
 /*
 Test Case 009 : search sunglasses - Handmade
@@ -21,22 +26,21 @@ public class TC009_UmitOlmez {
 
     @AfterClass
     public void quit(){
-        //driver.quit();
+        driver.quit();
     }
 
     @BeforeMethod
     public void setUp(){
-        driver = BrowserFactory.getDriver("chrome");
+        driver= BrowserFactory.getDriver("chrome");
     }
 
     @AfterMethod
     public void close(){
-       
-        //driver.close();
+        driver.close();
     }
 
     @Test
-    public void TC009(){
+    public void TC009() throws InterruptedException{
         //1.Go to Etsy.com
         driver.get("https://www.etsy.com");
         //2.Enter "sunglasses" in the search box
@@ -48,6 +52,32 @@ public class TC009_UmitOlmez {
         WebElement allFilters = driver.findElement(By.cssSelector("#search-filter-button"));
         allFilters.click();
         //4.Click on "Handmade" option under Item type title
+        WebElement handmade = driver.findElement(By.xpath("//label[@for='item-type-input-1']"));
+        handmade.click();
+        //5.Click on "Apply" button
+        WebElement apply = driver.findElement(By.xpath("//button[@*='Apply']"));
+        apply.click();
 
+        //wait for uploading the result
+        Thread.sleep(4000);
+
+        //6.Click and open the first product which is on the top-left, among results
+        WebElement firstProduct = driver.findElement(By.xpath("(//a[@data-listing-id='955159345']/div)[2]/div/h3"));
+        firstProduct.click();
+
+        //switching to the new tab
+        String currentWindowHandle = driver.getWindowHandle();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String handle : windowHandles) {
+
+            if(!handle.equals(currentWindowHandle)){
+                driver.switchTo().window(handle);
+            }
+        }
+
+        //7.Verify that "Handmade" expression is available under Highlights mini-title in the product detail page
+        WebElement handmade2 = driver.findElement(By.xpath("//div[@class='wt-ml-xs-2']"));
+        String actualResult = handmade2.getText();
+        Assert.assertTrue(actualResult.equals("Handmade"), "Verify that \"Handmade\" available");
     }
 }
