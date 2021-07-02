f2a2c142a6df19abeb8664a31e579c56ee922422
sumeyse <sumeyse@gmail.com>
2021-07-02 23:17:34 +0300

    tc004 started

---
+++ src/test/java/etsy/TC004_Sumeyse.java
@@ -0,0 +1,18 @@
+package etsy;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC004_Sumeyse {
+    /*Test Case 004 : Wedding & Party
+	1.Go to Etsy.com ( Chrome and Firefox only )
+	2.Click on "Wedding & Party" product category title
+	3.Click the inputbox the placeholder text of which is "Low" under Price option on the right side
+	4.Verify that "Custom" radio button is selected after inputbox is clicked*/
+
+
+
+}
