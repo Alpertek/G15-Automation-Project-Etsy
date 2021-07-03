296ff68aa3fd126ecb94bba89d63cecf1b3dbc09
Alpertek <alperen.yildirim7@gmail.com>
2021-07-03 22:09:51 +0300

    Test case 11 execution Edited

--- src/test/java/etsy/TC011_Alpertek.java
+++ src/test/java/etsy/TC011_Alpertek.java
@@ -15,36 +15,37 @@ public class TC011_Alpertek {
     WebDriver driver;
 
     @AfterClass
-    public void quit(){
+    public void quit() {
         driver.quit();
     }
 
     @BeforeMethod
-    public void setUp(){
-        driver= BrowserFactory.getDriver("chrome");
+    public void setUp() {
+        driver = BrowserFactory.getDriver("chrome");
     }
 
     @AfterMethod
-    public void close(){
+    public void close() {
         driver.close();
     }
     @Test
     public void executeTc011() throws InterruptedException {
-        this.driver.get("https://www.etsy.com");
-        WebElement vintage = this.driver.findElement(By.cssSelector("a[href^='https://www.etsy.com/c/vintage']"));
+        driver.get("https://www.etsy.com");
+        WebElement vintage = driver.findElement(By.cssSelector("a[href^='https://www.etsy.com/c/vintage']"));
         vintage.click();
         Thread.sleep(3000);
-        WebElement shippingOptions = this.driver.findElement(By.id("ship_to_select"));
+        WebElement shippingOptions = driver.findElement(By.id("ship_to_select"));
         Select selectShippingOptions = new Select(shippingOptions);
         selectShippingOptions.selectByVisibleText("China");
         Thread.sleep(3000);
         String expectedOption = "China";
         // To prevent an exception that says "element is not attached to the page document" :
-        shippingOptions = this.driver.findElement(By.id("ship_to_select"));
+        shippingOptions = driver.findElement(By.id("ship_to_select"));
         selectShippingOptions = new Select(shippingOptions);
         String actualOption = selectShippingOptions.getFirstSelectedOption().getText();
         Assert.assertEquals(actualOption, expectedOption, "Verify that 'China' is selected");
-        boolean containsCN = this.driver.getCurrentUrl().contains("ship_to=CN");
+        boolean containsCN = driver.getCurrentUrl().contains("ship_to=CN");
         Assert.assertTrue(containsCN, "Verify that the current url contains 'ship_to=CN'");
     }
 }
+
